5c51ef9a849565b53751fd27639e64fd9ecb03e8
Wasim Alam <wasimalam072@gmail.com>
2023-11-17 13:09:36 +0530

    Firebase Push Notification code push

--- XamarinApp.Android/MainActivity.cs
+++ XamarinApp.Android/MainActivity.cs
@@ -1,9 +1,9 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Plugin.FirebasePushNotification;
+using Android.Content;
 
 namespace XamarinApp.Droid
 {
@@ -17,12 +17,19 @@ namespace XamarinApp.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            FirebasePushNotificationManager.ProcessIntent(this, Intent);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            FirebasePushNotificationManager.ProcessIntent(this, intent);
         }
     }
 }
--- XamarinApp/App.xaml.cs
+++ XamarinApp/App.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Plugin.FirebasePushNotification;
+using System;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace XamarinApp
 {
@@ -11,6 +11,8 @@ namespace XamarinApp
             InitializeComponent();
 
             MainPage = new MainPage();
+
+            FirebasePushNotificationMethod();
         }
 
         protected override void OnStart()
@@ -23,6 +25,64 @@ namespace XamarinApp
 
         protected override void OnResume()
         {
+        }
+
+        protected void FirebasePushNotificationMethod()
+        {
+            /// <summary>
+            /// Event triggered when token is refreshed
+            /// </summary>
+            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"TOKEN : {p.Token}");
+            };
+
+            /// <summary>
+            /// Event triggered when a notification is received
+            /// </summary>
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Received");
+            };
+
+            /// <summary>
+            /// Event triggered when a notification is opened
+            /// </summary>
+            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Opened");
+                foreach (var data in p.Data)
+                {
+                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+                }
+            };
+
+            /// <summary>
+            /// Event triggered when there's an error
+            /// </summary>
+            CrossFirebasePushNotification.Current.OnNotificationAction += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Action");
+
+                if (!string.IsNullOrEmpty(p.Identifier))
+                {
+                    System.Diagnostics.Debug.WriteLine($"ActionId: {p.Identifier}");
+                    foreach (var data in p.Data)
+                    {
+                        System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+                    }
+
+                }
+            };
+
+            /// <summary>
+            /// Event triggered when Notification Delete
+            /// </summary>
+            CrossFirebasePushNotification.Current.OnNotificationDeleted += (s, p) => 
+            {
+                System.Diagnostics.Debug.WriteLine("Deleted");
+            }; 
+
         }
     }
 }
