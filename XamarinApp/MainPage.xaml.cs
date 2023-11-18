91bd1ad7f081ba998b9b4b613d4c776aa90ccfeb
Wasim Alam <wasimalam072@gmail.com>
2023-11-19 01:16:49 +0530

    Bar Code and QR Code Implemented

--- XamarinApp/MainPage.xaml.cs
+++ XamarinApp/MainPage.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using ZXing;
 
 namespace XamarinApp
 {
@@ -13,6 +9,48 @@ namespace XamarinApp
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainViewModel();
+
+            // https://blog.verslu.is/xamarin/xamarin-forms-xamarin/zxing-android-skipping-frames/
+            ScannerView.Options.DelayBetweenAnalyzingFrames = 5; // 5 milliseconds, and lower than the default - weird that it's better
+            ScannerView.Options.DelayBetweenContinuousScans = 2000; //2000
+            ScannerView.Options.InitialDelayBeforeAnalyzingFrames = 300;
+            ScannerView.Options.TryHarder = false;
+            ScannerView.Options.TryInverted = false;
+            ScannerView.Options.AutoRotate = false;
+            ScannerView.Options.UseFrontCameraIfAvailable = false;
+            ScannerView.Options.PossibleFormats.Clear();
+            ScannerView.Options.PossibleFormats.Add(BarcodeFormat.CODE_128);
+            ScannerView.Options.PossibleFormats.Add(BarcodeFormat.QR_CODE);
+        }
+
+        private bool _isFirstTorchOnRequest = true;
+
+        private void ScannerOverlay_FlashButtonClicked(Button sender, EventArgs e)
+        {
+            if (_isFirstTorchOnRequest) // else won't turn on with 1st click
+            {
+                ScannerView.IsTorchOn = true;
+                _isFirstTorchOnRequest = false;
+            }
+            else
+            {
+                ScannerView.IsTorchOn = !ScannerView.IsTorchOn;
+            }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ScannerView.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            ScannerView.IsScanning = false;
         }
     }
 }
---
+++ XamarinApp/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using ZXing;
+
+namespace XamarinApp
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public ICommand ScanResultCommand { get; private set; }
+        private bool _doScan = true;
+        public MainViewModel()
+        {
+            ScanResultCommand = new Command(ScanResult);
+        }
+
+        private bool _isAnalyzing = true; // start with analysis on
+        public bool IsAnalyzing
+        {
+            get => _isAnalyzing;
+            set => SetProperty(ref _isAnalyzing, value);
+        }
+
+        private Result _result;
+        public Result Result
+        {
+            get => _result;
+            set => SetProperty(ref _result, value);
+        }
+
+        private string _barCodeValue;
+
+        public string BarCodeValue
+        {
+            get { return _barCodeValue; }
+            set { _barCodeValue = value; }
+        }
+
+        /// <summary>
+        /// Unlike your typical button command, this methods is called from the hardware, on a non-UI thread
+        /// </summary>
+        private void ScanResult()
+        {
+            //if (!_doScan) // use this non-UI property to block re-entry, as this call comes in on a non-UI thread
+            //{
+            //    return;
+            //}
+
+            //_doScan = false; // doing this immediately, in this thread, to really avoid re-entry
+
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                IsAnalyzing = false; // while could set _doScane false earlier, must call the rest from UI thread
+                //var barcode = Result.Text;  // not exactly what I do, but same principle.
+                BarCodeValue = Result.Text;  // not exactly what I do, but same principle.
+                _ = Application.Current.MainPage.DisplayAlert("Bar Code", BarCodeValue, "Cancel");
+            });
+
+            // NOTE:  DO NOT set _doScan = true here, because this code runs in separate thread from Main UI thread, BEFORE GoBackWithBarcodeAsync completes.
+        }
+    }
+}
