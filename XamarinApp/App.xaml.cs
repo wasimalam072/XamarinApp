5bf7ce5dc2b8b9bdcef236f81dc6edc111f02ab6
Wasim Alam <wasimalam072@gmail.com>
2023-11-18 18:58:45 +0530

    App Center Crashes integration Android and iOS

--- XamarinApp/App.xaml.cs
+++ XamarinApp/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,14 +18,23 @@ namespace XamarinApp
 
         protected override void OnStart()
         {
+            AppCenterCrashMethod();
         }
 
         protected override void OnSleep()
         {
+
         }
 
         protected override void OnResume()
         {
+            AppCenterCrashMethod();
+        }
+
+        protected void AppCenterCrashMethod()
+        {
+            AppCenter.Start("android=55578713-d2b3-4c4c-abb4-a0f2830da90c;" +
+                  "ios=4c82566e-b3a5-4197-b96b-5485d831983f;", typeof(Analytics), typeof(Crashes));
         }
     }
 }
--- XamarinApp/MainPage.xaml.cs
+++ XamarinApp/MainPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,21 @@ namespace XamarinApp
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                //Console.WriteLine("Click here");
+                int i = 10, j = 0;
+                int result = i / j;
+                Analytics.TrackEvent("Button_Clicked");
+            }
+            catch (Exception Ex)
+            {
+                Crashes.TrackError(Ex);
+            }
         }
     }
 }
